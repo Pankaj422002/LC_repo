518afe3946c0175d5994cc099f42d0a97e23bf14
Pankaj422002 <pankajjoshirpvv@gmail.com>
2024-10-02 17:47:54 +0530

    MaxScoreRemoveStones.cpp added

---
+++ DS_STL_Container/MaxScoreRemoveStones.cpp
@@ -0,0 +1,102 @@
+/* JAI SHREE RAM */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ONLINE_JUDGE
+#define int             long long int
+#define F               first
+#define S               second
+#define pb              push_back
+#define si              set<int>
+#define vi              vector<int>
+#define pii             pair<int,int>
+#define vpi             vector<pii>
+#define vpp             vector<pair<int,pii>>
+#define mii             map<int,int>
+#define mpi             map<pii, int>
+#define spi             set<pii>
+#define endl            "\n"
+#define sz(x)           ((int) x.size())
+#define all(p)          p.begin(), p.end()
+#define double          long double
+#define que_max         priority_queue<int>
+#define que_min         priority_queue<int, vi, greater<int>>
+#define bug(...)        __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)        for(auto x: a)cout<<x<<" ";cout<<endl
+#define print1(a)       for(auto x: a)cout<<x.F<<" "<<x.S<<endl
+#define print2(a,x,y)   for(int i=x;i<y;i++)cout<<a[i]<<" ";cout<<endl
+
+inline int power(int a, int b){
+    int x=1;
+    while(b){
+        if(b&1)x=x*a;
+        a=a*a;
+        b=b>>1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout<< name << " : " << arg1<<endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names+1, ',');
+    cout.write(names, comma - names) << " : "<<arg1<<" | "; __f(comma+1, args...);
+}
+
+const int N = 200005;
+
+int maximumScore(int a, int b, int c) {
+    
+    que_max max_heap;
+    max_heap.push(a);
+    max_heap.push(b);
+    max_heap.push(c);
+
+    int cnt=0;
+    while(max_heap.size()>=2){
+    	
+    	int f=0,s=0;
+    	if(!max_heap.empty()){
+    		f=max_heap.top()-1;max_heap.pop();
+    	}
+    	if(!max_heap.empty()){
+    		s=max_heap.top()-1;max_heap.pop();
+    	}
+    	if(f>0)max_heap.push(f);    	
+    	if(s>0)max_heap.push(s);
+    	cnt++;
+    }
+    return cnt;
+
+}
+
+
+void solve(){
+    
+	int a,b,c;cin>>a>>b>>c;
+	cout<<maximumScore(a,b,c)<<endl;
+
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt","r",stdin);
+    freopen("../output.txt","w",stdout);
+#endif
+
+    clock_t z = clock();
+
+    int t=1;
+    cin>>t;
+    while(t--)solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+}
