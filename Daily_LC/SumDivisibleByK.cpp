4c84ffaf46c91f49007cf7200a43a280cd042060
Pankaj422002 <pankajjoshirpvv@gmail.com>
2024-10-04 15:01:40 +0530

    SumDivisibleByK added

---
+++ Daily_LC/SumDivisibleByK.cpp
@@ -0,0 +1,104 @@
+// SumDivisibleByK.cpp
+/* JAI SHREE RAM */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ONLINE_JUDGE
+#define int             long long int
+#define F               first
+#define S               second
+#define pb              push_back
+#define si              set<int>
+#define vi              vector<int>
+#define pii             pair<int,int>
+#define vpi             vector<pii>
+#define vpp             vector<pair<int,pii>>
+#define mii             map<int,int>
+#define mpi             map<pii, int>
+#define spi             set<pii>
+#define endl            "\n"
+#define sz(x)           ((int) x.size())
+#define all(p)          p.begin(), p.end()
+#define double          long double
+#define que_max         priority_queue<int>
+#define que_min         priority_queue<int, vi, greater<int>>
+#define bug(...)        __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)        for(auto x: a)cout<<x<<" ";cout<<endl
+#define print1(a)       for(auto x: a)cout<<x.F<<" "<<x.S<<endl
+#define print2(a,x,y)   for(int i=x;i<y;i++)cout<<a[i]<<" ";cout<<endl
+
+inline int power(int a, int b){
+    int x=1;
+    while(b){
+        if(b&1)x=x*a;
+        a=a*a;
+        b=b>>1;
+    }
+    return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout<< name << " : " << arg1<<endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names+1, ',');
+    cout.write(names, comma - names) << " : "<<arg1<<" | "; __f(comma+1, args...);
+}
+
+const int N = 200005;
+
+int sumDivisibleByK(vector<int> &arr, int p){
+	unordered_map<int,int> mp;
+	int n=arr.size(),total=0;
+	for(auto it: arr)total+=it;
+
+	if(total%p==0)return 0;
+	
+	int target=total%p, ans=n;
+
+	int curr=0;
+	mp[0]=-1;
+	for(int i=0;i<n;i++){
+		curr+=arr[i];
+		int currMod=curr%p;
+		int temp=(currMod-target+p)%p;
+
+		if(mp.find(temp)!= mp.end()){
+			ans = min(ans, i-mp[temp]);
+		}
+
+		mp[currMod]=i;
+	}
+	return ans==n?-1:ans;
+}
+
+void solve(){
+    int n,p;cin>>n>>p;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+
+    cout<<sumDivisibleByK(arr,p)<<endl;
+
+}
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt","r",stdin);
+    freopen("../output.txt","w",stdout);
+#endif
+
+    clock_t z = clock();
+
+    int t=1;
+    cin>>t;
+    while(t--)solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+}
