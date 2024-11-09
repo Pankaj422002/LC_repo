4e99d4386150fabe16360f2fc67ee8ada11d1cae
formalpankaj1 <formalpankaj1@gmail.com>
2024-11-09 17:56:20 +0530

    CP/MagicLocker.cpp added

---
+++ CP/MagicLocker.cpp
@@ -0,0 +1,102 @@
+// MagicLocker.cpp
+/* JAI SHREE RAM */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ONLINE_JUDGE
+// #define int 			long long int
+#define F 				first
+#define S 				second
+#define pb 				push_back
+#define si 				set<int>
+#define vi 				vector<int>
+#define pii 			pair<int,int>
+#define vpi 			vector<pii>
+#define vpp 			vector<pair<int,pii>>
+#define mii 			map<int,int>
+#define mpi 			map<pii, int>
+#define spi 			set<pii>
+#define endl 			"\n"
+#define sz(x) 			((int) x.size())
+#define all(p) 			p.begin(), p.end()
+#define double 			long double
+#define que_max 		priority_queue<int>
+#define que_min			priority_queue<int, vi, greater<int>>
+#define bug(...) 		__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a) 		for(auto x: a)cout<<x<<" ";cout<<endl
+#define print1(a) 		for(auto x: a)cout<<x.F<<" "<<x.S<<endl
+#define print2(a,x,y) 	for(int i=x;i<y;i++)cout<<a[i]<<" ";cout<<endl
+
+inline int power(int a, int b){
+	int x=1;
+	while(b){
+		if(b&1)x=x*a;
+		a=a*a;
+		b=b>>1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout<< name << " : " << arg1<<endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names+1, ',');
+	cout.write(names, comma - names) << " : "<<arg1<<" | "; __f(comma+1, args...);
+}
+
+const int N = 200005;
+
+const long long int mod = 1e9 + 7;
+
+long long int powr(int a, int b){
+    long long int ans=1, m=a;
+    while(b){
+        if(b&1)ans=ans*m,ans=ans%mod;
+        m=m*m;
+        m=m%mod;
+        b>>=1;
+    }
+    return ans;
+}
+
+int locker(int n){
+
+    if(n==1||n==2||n==3)return n;
+    
+    if(n%3 == 0){
+        return powr(3,n/3)%mod;
+    }else if(n%3 == 1){
+        return ((powr(3,(n/3)-1)%mod)*4)%mod;
+    }else if(n%3 == 2){
+        return ((powr(3,(n-2)/3)%mod)*2)%mod;
+    }
+    
+}
+
+void solve(){
+	int n;cin>>n;
+	cout<<locker(n)<<endl;
+}
+
+int32_t main(){
+
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt","r",stdin);
+	freopen("../output.txt","w",stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t=1;
+	cin>>t;
+	while(t--)solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+	return 0;
+}
