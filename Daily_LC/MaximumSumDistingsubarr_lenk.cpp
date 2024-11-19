349316ab1ff31cd857a0ef6321f87ec676c18d31
Pankaj422002 <pankajjoshirpvv@gmail.com>
2024-11-19 19:45:15 +0530

    Daily_LC/MaximumSumDistingsubarr_lenk.cpp added

---
+++ Daily_LC/MaximumSumDistingsubarr_lenk.cpp
@@ -0,0 +1,115 @@
+/* JAI SHREE RAM */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ONLINE_JUDGE
+#define int             long long int
+#define F               first
+#define S               second
+#define pb              push_back
+#define si              set<int>
+#define vi              vector<int>
+#define pii             pair<int,int>
+#define vpi             vector<pii>
+#define vpp             vector<pair<int,pii>>
+#define mii             map<int,int>
+#define mpi             map<pii, int>
+#define spi             set<pii>
+#define endl            "\n"
+#define sz(x)           ((int) x.size())
+#define all(p)          p.begin(), p.end()
+#define double          long double
+#define que_max         priority_queue<int>
+#define que_min         priority_queue<int, vi, greater<int>>
+#ifndef ONLINE_JUDGE
+#define bug(...)        __f (#__VA_ARGS__, __VA_ARGS__)
+#endif
+#define print(a)        for(auto x: a)cout<<x<<" ";cout<<endl
+#define print1(a)       for(auto x: a)cout<<x.F<<" "<<x.S<<endl
+#define print2(a,x,y)   for(int i=x;i<y;i++)cout<<a[i]<<" ";cout<<endl
+
+inline int power(int a, int b){
+    int x=1;
+    while(b){
+        if(b&1)x=x*a;
+        a=a*a;
+        b=b>>1;
+    }
+    return x;
+}
+
+#ifndef ONLINE_JUDGE
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout<< name << " : " << arg1<<endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names+1, ',');
+    cout.write(names, comma - names) << " : "<<arg1<<" | "; __f(comma+1, args...);
+}
+#endif
+
+const int N = 200005;
+
+long long maximumSubarraySum(vector<int>& nums, int k) {
+    unordered_map<int,int> mp;
+    int i=0,j=0,n=nums.size();
+    long long ans=0,sum=0;
+    while(j<n){
+        //calculation
+        mp[nums[j]]++;
+        sum+=nums[j];
+
+        if(j-i+1<k)j++;
+        else if(j-i+1 == k){
+            //ans<==calculation :
+            if(mp.size() == k){
+                ans=max(ans, sum);
+            }
+
+            //slide the window:
+            sum=sum-nums[i];
+            mp[nums[i]]--;
+            if(mp[nums[i]]==0)mp.erase(mp.find(nums[i]));
+
+            i++;j++;
+        }
+
+    }
+    return ans;
+}
+
+
+
+void solve(){
+   //your code
+   int n,k;cin>>n>>k;
+   vector<int> arr(n);
+   for(int i=0;i<n;i++)cin>>arr[i];
+    
+   cout<<maximumSubarraySum(arr,k)<<endl;
+
+}
+
+int32_t main(){
+
+#ifndef ONLINE_JUDGE
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("../input.txt","r",stdin);
+    freopen("../output.txt","w",stdout);
+    clock_t z = clock();
+#endif
+
+    int t=1;
+    cin>>t;
+    while(t--)solve();
+
+#ifndef ONLINE_JUDGE
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+#endif
+
+
+    return 0;
+}
+
