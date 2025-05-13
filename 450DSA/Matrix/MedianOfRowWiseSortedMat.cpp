71c4fb7fcc4fa237fba114619d7cabd1ec773a8f
Pankaj422002 <pankajjoshirpvv@gmail.com>
2025-05-13 20:46:10 +0530

    changes done

---
+++ 450DSA/Matrix/MedianOfRowWiseSortedMat.cpp
@@ -0,0 +1,110 @@
+/* JAI SHREE RAM */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ONLINE_JUDGE
+#define int             long long int
+#define F               first
+#define S               second
+#define pb              push_back
+#define si              set<int>
+#define vi              vector<int>
+#define pii             pair<int,int>
+#define vpi             vector<pii>
+#define vpp             vector<pair<int,pii>>
+#define mii             map<int,int>
+#define mpi             map<pii, int>
+#define spi             set<pii>
+#define endl            "\n"
+#define sz(x)           ((int) x.size())
+#define all(p)          p.begin(), p.end()
+#define double          long double
+#define que_max         priority_queue<int>
+#define que_min         priority_queue<int, vi, greater<int>>
+#ifndef ONLINE_JUDGE
+#define bug(...)        __f (#__VA_ARGS__, __VA_ARGS__)
+#endif
+#define print(a)        for(auto x: a)cout<<x<<" ";cout<<endl
+#define print1(a)       for(auto x: a)cout<<x.F<<" "<<x.S<<endl
+#define print2(a,x,y)   for(int i=x;i<y;i++)cout<<a[i]<<" ";cout<<endl
+#define ppi             pair<int,pair<int,int>>
+
+inline int power(int a, int b){
+    int x=1;
+    while(b){
+        if(b&1)x=x*a;
+        a=a*a;
+        b=b>>1;
+    }
+    return x;
+}
+
+#ifndef ONLINE_JUDGE
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout<< name << " : " << arg1<<endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names+1, ',');
+    cout.write(names, comma - names) << " : "<<arg1<<" | "; __f(comma+1, args...);
+}
+#endif
+
+const int N = 200005;
+
+int FindMedian(vector<vector<int>> &mat){
+    int n=mat.size(),m=mat[0].size();
+
+    priority_queue<ppi,vector<ppi>,greater<ppi>> min_heap;
+    for(int i=0;i<n;i++)min_heap.push({mat[i][0],{i,0}});
+
+    int k = (n*m)/2 , cnt=0;
+    while(!min_heap.empty()){
+
+        ppi temp = min_heap.top();
+        int ele = temp.F;
+        int i = temp.S.F, j = temp.S.S;
+
+        cnt++;
+
+        if(cnt==k+1)return ele;
+
+        if(j+1 < m){
+            min_heap.push({mat[i][j+1],{i,j+1}});
+        }
+
+    }
+    return -1;
+}
+
+
+void solve(){
+    int n,m;cin>>n>>m;
+    vector<vector<int>> mat(n, vector<int>(m));
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)cin>>mat[i][j];
+    cout<<FindMedian(mat)<<endl;
+}
+
+int32_t main(){
+
+#ifndef ONLINE_JUDGE
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("../../input.txt","r",stdin);
+    freopen("../../output.txt","w",stdout);
+    clock_t z = clock();
+#endif
+
+    int t=1;
+    cin>>t;
+    while(t--)solve();
+
+#ifndef ONLINE_JUDGE
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+#endif
+
+
+    return 0;
+}
+
